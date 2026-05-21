7d9f19ff9d71998571e044d48d9fac7f660c2249
hajar <hajar.htiti.ing@gmail.com>
2026-05-21 23:45:05 +0200

    jumpscareTrig

---
+++ Assets/jumpscareTrig.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class jumpscareTrig : MonoBehaviour
+{
+    public GameObject playerObj, jumpscareCam, ambianceLayers;
+    public Animator monsterAnim;
+    public string sceneName;
+    public float jumpscareTime;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerObj.SetActive(false);
+            jumpscareCam.SetActive(true);
+            ambianceLayers.SetActive(false);
+            StartCoroutine(changeScene());
+            monsterAnim.SetTrigger("jumpscare");
+        }
+    }
+
+    IEnumerator changeScene()
+    {
+        yield return new WaitForSeconds(jumpscareTime);
+        SceneManager.LoadScene(sceneName); // Correction ici !
+    }
+}
